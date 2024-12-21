a952a2fdf1fed4f3503879953ff1dabb3be6c796
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-12-22 02:26:52 +0500

    React JS Assignment 08 completed

---
+++ src/App.jsx
@@ -0,0 +1,369 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div>
+
+    {/* Navbar Section Start */}
+
+      <div className='Main-Navbar-Top'>
+        <div className='Navbar-Div-1'>
+          <ul className='First-List'>
+            <li className='realme-Navbar-Heading'>realme</li>
+          </ul>
+          <ul className='Second-List'>
+            <li>realme smartphone</li>
+            <li>narzo smartphone</li>
+            <li>Audio</li>
+            <li>Accessories</li>
+          </ul>
+        </div>
+        <div className='Navbar-Div-2'>
+          <ul className='Third-List'>
+            <li>Brand</li>
+            <li>Support</li>
+            <li>Community</li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Navbar Section End */}
+
+
+      {/* Image Section Start */}
+
+      <div>
+        <img className='Image' src="https://image01.realme.net/general/20241212/1733967719985cc58ce4f90a543a8bb330349fb297458.jpg.webp" alt="" />
+      </div>
+
+      {/* Image Section End */}
+
+
+      {/* SmartPhones Section Start */}
+
+      <div>
+        <div className='Main-Div-Card-Section'>
+          <div>
+            <h1>SmartPhones</h1>
+          </div>
+          <div className='Main-Card-Section'>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme C75</h2>
+                    <p className='Card-Details'>Dhulai Horai Hai</p>
+                  </div>
+                  <div>
+                   <p></p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20241212/1733967866096fa2982967549485db8c3259a0f9a1fe7.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme 13 + 5G</h2>
+                    <p className='Card-Details'>Speed to Victory</p>
+                  </div>
+                  <div>
+                   <p>Rs.89,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20241108/1731062061605d960daec82634da8ae53a2b5bba2b081.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme GT 6</h2>
+                    <p className='Card-Details'>Power meets AI</p>
+                  </div>
+                  <div>
+                   <p>Rs.149,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20240930/17276898714837028c74b7ec64f578595a0bec73bd213.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme Note 60</h2>
+                    <p className='Card-Details'>Champion Quality</p>
+                  </div>
+                  <div>
+                   <p>Rs.26,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20240919/17267273214814cc35277c25042d580c622fec0dad738.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme C61</h2>
+                    <p className='Card-Details'>Hard to Break</p>
+                  </div>
+                  <div>
+                   <p>Rs.32,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20240827/17247517057780f136750f9ab445193022ca17ab4b885.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme C65</h2>
+                    <p className='Card-Details'>Unbreakable Champion</p>
+                  </div>
+                  <div>
+                   <p>Rs.49,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20240726/17219881569684931245943754b279da88be96c1ba769.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme 12</h2>
+                    <p className='Card-Details'>Power in every shot</p>
+                  </div>
+                  <div>
+                   <p>Rs.59,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20240624/171922460925609665c170ea145d0a40db9866d4a1d51.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme 12 + 5G</h2>
+                    <p className='Card-Details'>Power in every shot</p>
+                  </div>
+                  <div>
+                   <p>Rs.74,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20240620/1718869504893402543bcb87c4708aedfe7ce612e8725.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme C63</h2>
+                    <p className='Card-Details'>3 min Charge, 60 min T20 Match</p>
+                  </div>
+                  <div>
+                   <p>Rs.39,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20240607/1717742184050e38a613e33f146b49dfa47720aea45cf.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme Note 50</h2>
+                    <p className='Card-Details'>Long-Lasting Value Beast</p>
+                  </div>
+                  <div>
+                   <p>Rs.24,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20240221/170850139998146c2183ef4824e00ab6ed688ebc55037.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme C67</h2>
+                    <p className='Card-Details'>108MP 3x In-sensor Zoom Camera, Snapdragon 685</p>
+                  </div>
+                  <div>
+                   <p>Rs.39,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20240105/1704419834105be51d94716d4469084d9ba9b1a904eb6.png" alt="" />
+                </div>
+            </div>
+            <div className='Cards'>
+                <div>
+                  <div>
+                    <small className='Card-New-Box'>NEW</small>
+                    <h2>realme C53</h2>
+                    <p className='Card-Details'>Champion Gold, 33W Champion Charge</p>
+                  </div>
+                  <div>
+                   <p>Rs.39,999</p>
+                  </div>
+                </div>
+                <div>
+                  <img className='Card-Image' src="https://image01.realme.net/general/20231129/17012403962026a15564d103748a6931a17233c223eb7.png" alt="" />
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Smartphones Section End */}
+
+
+      {/* Accessories Section Start */}
+
+      <div>
+      <div className='Main-Div-Card-Section'>
+          <div>
+            <h1>Accessories</h1>
+          </div>
+          <div className='Main-Card-Section'>
+            <div className='Cards'>
+              <div>
+                <div>
+                  <small className='Card-New-Box'>NEW</small>
+                  <h2>realme Buds T110</h2>
+                  <p className='Card-Details'>Listen in Full Color</p>
+                </div>
+                 <div>
+                 <p>Rs.4,999</p>
+                </div>
+              </div>
+              <div>
+                <img className='Card-Image' src="https://image01.realme.net/general/20240628/1719540050662f1db6cdfd9104e5896203287689d7d2d.png" alt="" />
+              </div>
+           </div>
+           <div className='Cards'>
+              <div>
+                <div>
+                   <h2>realme Buds Q</h2>
+                  <p className='Card-Details'>Music Never Ends</p>
+                </div>
+                <div>
+                  <p>Rs.7,999</p>
+                </div>
+              </div>
+              <div>
+               <img className='Card-Image' src="https://image01.realme.net/general/20201111/1605080175371.jpg" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Accessories Section End */}
+
+
+      {/* Footer Section Start */}
+      
+      <div>
+        <div className='Footer-Section'>
+          <div className='Footer-Section2'>
+            <div className='Footer-Lists-Section'>
+              <div>
+                <h4>Recommended</h4>
+              </div>
+              <div className='Footer-Lists'>
+                <p className='Small-Lines'>realme C75</p>
+                <p className='Small-Lines'>realme 13+ 5G</p>
+                <p className='Small-Lines'>realme Note 60</p>
+                <p className='Small-Lines'>realme C61</p>
+                <p className='Small-Lines'>realme C65</p>
+                <p className='Small-Lines'>realme 12</p>
+                <p className='Small-Lines'>realme 12+ 5G</p>
+              </div>
+            </div>
+            <div className='Footer-Lists-Section'>
+              <div>
+                <h4>Support</h4>
+              </div>
+              <div className='Footer-Lists'>
+                <p className='Small-Lines'>FAQ</p>
+                <p className='Small-Lines'>Troubleshooting</p>
+              </div>
+            </div>
+            <div className='Footer-Lists-Section'>
+              <div>
+                <h4>About realme</h4>
+              </div>
+              <div className='Footer-Lists'>
+                <p className='Small-Lines'>Our Brand</p>
+                <p className='Small-Lines'>Newsroom</p>
+              </div>
+            </div>
+            <div className='Footer-Lists-Section'>
+              <div>
+                <h4>Contact realme</h4>
+              </div>
+              <div className='Footer-Lists'>
+                <p className='Small-Lines'>Whastapp</p>
+                <p className='Small-Lines'>service.pk@realme.com</p>
+              </div>
+            </div>
+            <div className='Footer-Lists-Section'>
+              <div>
+                <h2>04238048018</h2>
+              </div>
+              <div className='Footer-Lists'>
+                <p className='Small-Lines'>09:30 - 18:00, MON - SAT</p>
+                <p className='Small-Lines'>Exclude Holidays</p>
+              </div>
+              <br />
+              <div className='Footer-Logo-Div'>
+                <img className='Footer-Logo' src="src\icons\facebook logo.png" alt="" />
+                <img className='Footer-Logo' src="src\icons\twitter logo.png" alt="" />
+                <img className='Footer-Logo' src="src\icons\youtube logo.png" alt="" />
+                <img className='Footer-Logo' src="src\icons\instagram logo.png" alt="" />
+              </div>
+            </div>
+          </div>
+          <br /><hr /><br />
+          <div className='Footer-2nd-Section'>
+            <div className='Footer-2nd-Section-Part1'>
+              <small>Pakistan ( English / PKR )</small>
+              <small className='Light-Name'>@ 2019-2024 realme. All Rights Reserved.</small>
+            </div>
+            <div className='Footer-2nd-Section-Part2'>
+              <small className='Light-Name'>User Agreement &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; Privacy Policy &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; Terms of Sales &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; Warranty Terms</small>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Footer Section End */}
+
+    </div>
+  
+ )
+
+}
+
+export default App
